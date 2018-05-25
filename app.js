e429a74116c53b30f8e5af35f5861fca6a8e4b16
Travis L Rutherford <trutherford2388@icloud.com>
2018-05-25 10:51:24 -0400

    Initial Setup

---
+++ app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealthBar: 100,
+    monsterHealthBar: 100,
+    attackLog: []
+  },
+  methods: {
+    newGame: function() {},
+    attack: function() {},
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {},
+    monsterAttack: function() {},
+    generaterNumber: function(lowestNumberInRange, highestNumberInRange) {}
+  }
+});
